2b59bcb73b375dbd4fdbd9fc9ffdcb57d6caa97d
AerialKebab <vergil.hsu@gmail.com>
2019-10-02 18:23:52 -0700

    Figured out how to output to JSON, need to work on updating the songList

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import sys
 import spotipy
 import spotipy.util as util
 import os
+from os import path
 from selenium import webdriver
 from spotipy import oauth2
+import json
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -14,8 +16,53 @@ from selenium.webdriver.support import expected_conditions as EC
 client_id = "4eacb90864a74bc49e17a90d591723d7"
 client_secret = "f22221c21ede49e989f67e69ff5d4db6"
 redirect_uri = "http://localhost:3000/redirect"
-scope = 'user-library-read'
+scope = 'playlist-read-private playlist-read-collaborative'
 
+
+
+
+def update_playlist_entry(playlistJSON, isNew):
+    def insert_tracks_newPlaylist(tracks, songList):
+        for i, item in enumerate(tracks['items']):
+            track = item['track']
+            # print playlistJSON[playlist['name']][0][0]
+            songList.append({
+                track['id']: track['artists'][0]['name'] + " - " + track['name']
+            })
+
+            # if track['id'] not in playlistJSON[playlist['name']][1]:
+            #     playlistJSON[playlist['name']][0]['songs'].append({
+            #         track['id']: track['artists'][0]['name'] + " - " + track['name']
+            #     })
+                # print "added: " + track['id']
+
+    def insert_new_tracks(tracks, songList):
+        # for i, item in enumerate(tracks['items']):
+        #     track = item['track']
+        print songList[0]
+
+    playlistJSON[playlist['name']] = []
+    playlistJSON[playlist['name']].append({
+        'numOfSongs':playlist['tracks']['total']
+    })
+    songList = []
+    # print len(playlistJSON[playlist['name']])
+    results = sp.user_playlist(username, playlist['id'],fields="tracks,next")
+    tracks = results['tracks']
+
+
+    if isNew:
+        insert_tracks_newPlaylist(tracks,songList)
+        while tracks['next']:
+            tracks = sp.next(tracks)
+            insert_tracks_newPlaylist(tracks,songList)
+
+        playlistJSON[playlist['name']].append({
+            'songs': songList
+        })
+    else:
+        # insert_new_tracks(tracks,playlistJSON[playlist['name']][0])
+        print playlistJSON
 
 if len(sys.argv) > 1:
     username = sys.argv[1]
@@ -46,14 +93,43 @@ if not token_info:
 if token_info:
     token = token_info['access_token']
 
-# token = prompt_for_user_token(username, scope)
-
 
 if token:
     sp = spotipy.Spotify(auth=token)
-    results = sp.current_user_saved_tracks()
-    for item in results['items']:
-        track = item['track']
-        print track['name'] + ' - ' + track['artists'][0]['name']
+    playlists = sp.user_playlists(username)
+
+
+
+    # playlistJSON = {}
+    if not path.exists("downloadLog.json"):
+        open("downloadLog.json","w+")
+
+    if os.stat("downloadLog.json").st_size != 0:
+        with open("downloadLog.json", "r") as input:
+            playlistJSON = json.load(input)
+    else:
+        playlistJSON = {}
+
+
+
+    for playlist in playlists['items']:
+        if playlist['owner']['id'] == username:
+            if playlist['name'] == "tester":
+                if playlist['name'] in playlistJSON:             #TODO: fix playlist name repeats, input (3) change to ID?
+                    print playlist['name']
+                    print '  total tracks', playlist['tracks']['total']
+                    print "playlist already following"
+                    if playlist['tracks']['total'] != playlistJSON[playlist['name']][0]['numOfSongs']:
+                        print " -- new songs added"                 #TODO: or removed!! (2)
+                    #     update_playlist_entry(playlistJSON,0)     #TODO: update playlist songList (1)
+                else:
+                    print "New playlist"
+                    update_playlist_entry(playlistJSON,1)
+
+
+    with open("downloadLog.json", "w+") as output:
+        json.dump(playlistJSON,output)
+
+
 else:
     print "Can't get token for", username
