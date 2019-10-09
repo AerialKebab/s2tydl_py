8aef4ee128a8c3d376d1540006bd8ed8dc0e0617
AerialKebab <vergil.hsu@gmail.com>
2019-10-09 16:38:41 -0700

    Finished threading the download state, such that GUI is correctly updated during background tasks

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from os import path
 from selenium import webdriver
 from spotipy import oauth2
 import json, operator
+import time
+
+from functools import partial
 
 from PyQt5 import QtCore, QtWidgets, QtGui
 from PyQt5.QtGui import *
@@ -31,14 +34,117 @@ from multiprocessing import Pool, Process
 #section modified from: https://github.com/plamere/spotipy/blob/master/spotipy/util.py#L18
 #and https://github.com/plamere/spotipy/blob/master/spotipy/oauth2.py
 
-def window():
+
+class downloadObject(QtCore.QObject):
+    finished = pyqtSignal()
+
+    def downloadPlaylists(self):
+        count = 0
+        while count < 3:
+            time.sleep(1)
+            print("Loop")
+            count += 1
+        self.finished.emit()
+
+    # def downloadPlaylists(self):
+    #     # print grid.itemAtPosition(1,0).widget().widget().layout().rowCount()
+    #     #QScrollArea/QGroupBox/QFormLayout (QFormLayout has rows)
+    #
+    #     playlistRows = self.grid.itemAtPosition(1,0).widget().widget().layout()
+    #
+    #     for i in range(playlistRows.rowCount()):
+    #         row = playlistRows.itemAt(i,0).widget()
+    #         playlistName = playlistRows.itemAt(i,1).widget().text()
+    #         if row.isChecked():
+    #             playlistJSON[playlistName][0]['toDownload'] = 1
+    #             updatePlaylistEntry(playlistJSON[playlistName][0]['playlistID'],playlistName)
+    #         else:
+    #             playlistJSON[playlistName][0]['toDownload'] = 0
+    #
+    #     with open("./downloadLogs/" + username + "downloadLog.json", "w+") as output:
+    #         json.dump(playlistJSON,output)
+    #
+    #     self.finished.emit()
+
+        # mainMenu(username,None)
+
+    def updatePlaylistEntry(playlistID, playlistName):
+        def insert_tracks_newPlaylist(tracks, songList):
+            for i, item in enumerate(tracks['items']):
+                track = item['track']
+
+                # print item['added_at']
+
+                songList.append({
+                    track['id']: track['artists'][0]['name'] + " - " + track['name'],
+                    'added_at': item['added_at']
+                })
+
+                trackName = track['artists'][0]['name'] + " - " + track['name']
+
+                # downloadVideo(findFirstYouTubeResult(track['artists'][0]['name'] + " - " + track['name']), trackName)
+
+        songList = []
+        results = sp.user_playlist(username, playlistID, fields="tracks,next")
+        tracks = results['tracks']
+
+        insert_tracks_newPlaylist(tracks,songList)
+        while tracks['next']:
+            tracks = sp.next(tracks)
+            insert_tracks_newPlaylist(tracks,songList)
+
+        songList.sort(key=operator.itemgetter('added_at'))
+        playlistJSON[playlistName][0]['songs'] = songList
+
+        # mainMenu(username, None)
+
+        # TODO
+        # if playlist['tracks']['total'] != 0:
+        #     playlistJSON[playlist['name']][0]['lastUpdated'] = playlistJSON[playlist['name']][1]['songs'][playlist['tracks']['total'] - 1]['added_at'] #lastelement
+
+    def downloadVideo(input,trackName):
+        path = './downloaded/' + trackName + '.%(ext)s'
+
+        # print path
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'noplaylist': True,
+            'audioformat': "mp3"
+        }
+        #
+        with youtube_dl.YoutubeDL({'outtmpl':path}) as ydl:
+            ydl.download([input])
+
+    def findFirstYouTubeResult(input):
+        url = "https://www.youtube.com/results?search_query=" + input
+        response = urllib.urlopen(url).read()
+        html = response.decode('utf-8')
+        soup = BeautifulSoup(html, 'html.parser')
+        for vid in soup.findAll(attrs={'class':'yt-uix-tile-link'}):
+            if vid['href'][0:9] == "/watch?v=":
+                return ('https://www.youtube.com' + vid['href'])
+                break;
+
+        # def __init__():
+
+
+class main(QtCore.QObject):
+    signalStartDownloadingPlaylists = QtCore.pyqtSignal()
+
     client_id = "4eacb90864a74bc49e17a90d591723d7"
     client_secret = "f22221c21ede49e989f67e69ff5d4db6"
     redirect_uri = "http://localhost:3000/redirect"
     scope = 'playlist-read-private playlist-read-collaborative'
     playlistJSON = None;
+    sp = None
 
-    def executeStates(gridIn):
+    app = QApplication(sys.argv)
+    win = QWidget()
+    grid = QGridLayout()
+
+    # thread = None
+
+    def executeStates(self, gridIn):
         grid = gridIn
         def mainMenu(username, token):
             def updateUsers():
@@ -56,12 +162,18 @@ def window():
 
             def getToken():
                 cache_path = "./caches/.cache-" + username
-                sp_oauth = oauth2.SpotifyOAuth(client_id, client_secret, redirect_uri, scope=scope, cache_path=cache_path)
+                sp_oauth = oauth2.SpotifyOAuth(self.client_id, self.client_secret, self.redirect_uri, scope=self.scope, cache_path=cache_path)
                 token_info = sp_oauth.get_cached_token()
                 token = token_info['access_token']
                 return token
 
             def getPlaylists(playlists):
+                def downloading():
+                    self.thread.start()
+                    grid.itemAtPosition(2,0).widget().setText("Downloading Playlists")
+                    grid.itemAtPosition(2,0).widget().setEnabled(False)
+                    #TODO: Disable checking playlists while downloading
+
                 playlistForm = QFormLayout()
                 scrollPlaylists = QtWidgets.QScrollArea()
                 groupBox = QGroupBox("Playlists")
@@ -74,6 +186,7 @@ def window():
                             'toDownload':0,
                             'numOfSongs':playlist['tracks']['total'],
                             'lastUpdated':0,
+                            'playlistID': playlist['id'],
                             'songs': []
                         })
                     playlistJSON[playlist['name']][0]['numOfSongs'] = playlist['tracks']['total']
@@ -92,74 +205,19 @@ def window():
 
                 # https://doc.qt.io/qt-5/qformlayout.html
 
+                self.worker = downloadObject()
+                self.thread = QtCore.QThread()
+                self.worker.moveToThread(self.thread)
+                self.worker.finished.connect(self.thread.quit)
+                self.thread.started.connect(self.worker.downloadPlaylists)
+                self.thread.finished.connect(partial(mainMenu,username,None))
+
+                # worker.moveToThread(self.thread)
+                # self.signalStartDownloadingPlaylists.connect(self.worker.printHello)
+
                 dl = QPushButton('Download Playlists')
-                dl.clicked.connect(downloadPlaylists)
+                dl.clicked.connect(downloading)
                 grid.addWidget(dl,2,0)
-
-            def downloadPlaylists():
-                # print grid.itemAtPosition(1,0).widget().widget().layout().rowCount()
-                #QScrollArea/QGroupBox/QFormLayout (QFormLayout has rows)
-                #TODO Downloading screen
-
-                playlistRows = grid.itemAtPosition(1,0).widget().widget().layout()
-
-                for i in range(playlistRows.rowCount()):
-                    row = playlistRows.itemAt(i,0).widget()
-                    playlistName = playlistRows.itemAt(i,1).widget().text()
-                    if row.isChecked():
-                        playlistJSON[playlistName][0]['toDownload'] = 1
-                        # updatePlaylistEntry(playlistName) #TODO
-                    else:
-                        playlistJSON[playlistName][0]['toDownload'] = 0
-
-                with open("./downloadLogs/" + username + "downloadLog.json", "w+") as output:
-                    json.dump(playlistJSON,output)
-
-            # def updatePlaylistEntry(playlistName):
-            #     def insert_tracks_newPlaylist(tracks, songList):
-            #         for i, item in enumerate(tracks['items']):
-            #             track = item['track']
-            #
-            #             # print item['added_at']
-            #
-            #             songList.append({
-            #                 track['id']: track['artists'][0]['name'] + " - " + track['name'],
-            #                 'added_at': item['added_at']
-            #             })
-            #
-            #             trackName = track['artists'][0]['name'] + " - " + track['name']
-            #             # print trackName
-            #             downloadVideo(findFirstYouTubeResult(track['artists'][0]['name'] + " - " + track['name']), trackName)
-            #
-            #     def insert_new_tracks(tracks, songList):
-            #         for i, item in enumerate(tracks['items']):
-            #             if item['added_at'] > playlistJSON[playlist['name']][0]['lastUpdated'] : ###
-            #                 track = item['track']
-            #                 songList.append({
-            #                     track['id']: track['artists'][0]['name'] + " - " + track['name'],
-            #                     'added_at': item['added_at']
-            #                 })
-            #                 playlistJSON[playlist['name']][0]['lastUpdated'] = item['added_at'];
-            #                 trackName = track['artists'][0]['name'] + " - " + track['name']
-            #                 print trackName
-            #
-            #                 downloadVideo(findFirstYouTubeResult(track['artists'][0]['name'] + " - " + track['name'] + "(Official Audio)"), trackName)
-            #
-            #     songList = []
-            #     results = sp.user_playlist(username, playlist['id'],fields="tracks,next")
-            #     tracks = results['tracks']
-            #
-            #     insert_tracks_newPlaylist(tracks,songList)
-            #     while tracks['next']:
-            #         tracks = sp.next(tracks)
-            #         insert_tracks_newPlaylist(tracks,songList)
-            #
-            #     songList.sort(key=operator.itemgetter('added_at'))
-            #     playlistJSON[playlistName]['songs'] = songList
-            #
-            #     # TODO
-            #     # if playlist['tracks']['total'] != 0:
-            #     #     playlistJSON[playlist['name']][0]['lastUpdated'] = playlistJSON[playlist['name']][1]['songs'][playlist['tracks']['total'] - 1]['added_at'] #lastelement
 
             clearGrid()
             grid.addWidget(QLabel("Select Playlists to Download"),0,0)
@@ -260,30 +318,25 @@ def window():
 
         enterSpotifyAcc()
 
-    def center():
-        frameGm = win.frameGeometry()
+    def center(self):
+        frameGm = self.win.frameGeometry()
         screen = QApplication.desktop().screenNumber(QApplication.desktop().cursor().pos())
         centerPoint = QApplication.desktop().screenGeometry(screen).center()
         frameGm.moveCenter(centerPoint)
-        win.move(frameGm.topLeft())
+        self.win.move(frameGm.topLeft())
 
-    app = QApplication(sys.argv)
-    win = QWidget()
-    grid = QGridLayout()
+    def window(self):
+        self.executeStates(self.grid)
+        self.win.setLayout(self.grid)
+        self.win.setWindowTitle("S2YTDL - by AerialKebab")
+        self.win.setGeometry(50,50,200,200)
+        self.win.show()
+        self.win.setMinimumSize(500, 400);
+        self.center()
+        sys.exit(self.app.exec_())
 
+    # if __name__ == '__main__':
+    #    window()
 
-    executeStates(grid)
-
-
-
-
-    win.setLayout(grid)
-    win.setWindowTitle("S2YTDL - by AerialKebab")
-    win.setGeometry(50,50,200,200)
-    win.show()
-    win.setMinimumSize(500, 400);
-    center()
-    sys.exit(app.exec_())
-
-if __name__ == '__main__':
-   window()
+start = main()
+start.window()
